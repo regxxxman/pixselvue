11d099848dbdc17191949437ee1ffefbd21d7c16
r3&mv^ <regxxxman@gmail.com>
2024-02-03 11:06:16 +0100

    new version

---
+++ assets/AboutView-0e5e06ec.js
@@ -0,0 +1,1 @@
+import{o as s,c as t,n as a,u as o,b as n,a as e}from"./index-a88e94c8.js";const c=e("div",null,null,-1),r=e("h1",{class:"pt-16"},"О нас",-1),l=[c,r],m={__name:"AboutView",setup(_){return(i,u)=>(s(),t("div",{style:a([{background:`url(${o(n)})`},{"background-size":"100% 100%"}]),class:"min-h-screen text-white"},l,4))}};export{m as default};
